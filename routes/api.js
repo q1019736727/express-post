c788a853b63c3a502d3620ae5eb2205a419602eb
Chiu Yung <1837060380@qq.com>
2019-02-21 17:22:14 +0800

    no message

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ const host_url = 'https://www.universelife.cn'
 //社区活动
 router.get('/v1.0/communityActCon/homePageList', function (req, res, next) {
     axios.get(host_url+'/activity/api/v1.0/communityActCon/homePageList', {}).then(function (response) {
-        console.log('---', response.data);
+        console.log(response.data);
         res.send(response.data)
     })
 })
@@ -18,8 +18,22 @@ router.get('/bannerCon/bannerShow', function (req, res, next) {
     axios.get(host_url+'/heli-oms/api/bannerCon/bannerShow', {
         params:code(req.query)
     }).then(function (response) {
+        console.log(response.data);
+        res.send(response.data)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+//activitylist
+router.get('/travel/travelActList',function (req,res,next) {
+    axios.get(host_url+'/heli-oms/api/travel/travelActList', {
+        params:code(req.query)
+    }).then(function (response) {
+        console.log(response.data);
         res.send(response.data)
     })
 })
+
 
 module.exports = router;
